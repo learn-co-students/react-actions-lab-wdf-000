fefba9201c19f7a20d27fb5f25bfdc6631eb303a
alexgriff <alexalexgriffith@gmail.com>
2016-10-15 23:46:12 -0400

    Done

--- actions/index.js
+++ actions/index.js
@@ -1,24 +1,79 @@
 'use strict';
 
 function addColumn (ev) {
+  ev.preventDefault();
+
+  let changedTable =
+    this.state.table.map( row => row.concat(['']));
+
+  this.setState({
+    table: changedTable
+  });
 }
 
 function addRow (ev) {
+  ev.preventDefault();
+
+  let changedTable =
+    this.state.table.concat([
+      this.state.table[0].map( col => "")
+      ]);
+
+  this.setState({
+    table: changedTable
+  });
 }
 
 function changeCell (rowIndex, columnIndex, ev) {
+  let changedTable =
+    this.state.table.slice();
+  changedTable[rowIndex][columnIndex] = ev.target.value
+
+  this.setState({
+    table: changedTable
+  });
 }
 
 function focusCell (rowIndex, columnIndex) {
+  let focused = [rowIndex, columnIndex];
+
+  this.setState({
+    focused: focused
+  });
 }
 
 function blurCell () {
+  let focused = null;
+
+  this.setState({
+    focused: focused
+  });
 }
 
 function removeRow (ev) {
+  ev.preventDefault();
+
+  let changedTable =
+    this.state.table.slice(0, -1);
+
+  if (changedTable.length > 1) {
+    this.setState({
+    table: changedTable
+    });
+  }
 }
 
 function removeColumn (ev) {
+  ev.preventDefault();
+  // if a row has more than one elem
+  if (this.state.table[0][1]) {
+    let changedTable =
+      this.state.table.map( row => row.slice(0, -1));
+
+    this.setState({
+      table: changedTable
+    });
+  }
 }
 
 module.exports = {
--- components/Table.js
+++ components/Table.js
@@ -4,7 +4,28 @@ const React = require('react');
 const Row = require('./Row');
 
 class Table extends React.Component {
+  constructor(props) {
+    super();
+
+  }
+
   render () {
+    return (
+      <table className="table" onBlur={ this.props.onBlur }>
+        <tbody>
+          {
+            this.props.table.map(
+              (row, i ) =>
+              <Row key={ i }
+                row={ row }
+                onChange={ this.props.onChange.bind(null, i) }
+                onFocus={ this.props.onFocus.bind(null, i) }
+                />
+            )
+          }
+        </tbody>
+      </table>
+    )
   }
 }
 
