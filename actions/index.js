371b8275dc93f91bd89174f9079078c9646e6f20
ckshei <cks856@gmail.com>
2016-10-27 12:28:34 -0400

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,24 +1,58 @@
 'use strict';
 
 function addColumn (ev) {
+  ev.preventDefault()
+  var newTable = this.state.table.map((row)=> [...row, ''])
+  this.setState({
+    table: newTable
+  })
 }
 
 function addRow (ev) {
+  ev.preventDefault()
+  this.setState({
+    table: [...this.state.table, this.state.table[0].map(()=> "")]
+  })
 }
 
 function changeCell (rowIndex, columnIndex, ev) {
+  var tableClone = this.state.table.slice()
+  tableClone[rowIndex][columnIndex] = ev.target.value
+  this.setState({
+    table: tableClone
+  })
 }
 
 function focusCell (rowIndex, columnIndex) {
+  this.setState({
+    focused: [rowIndex, columnIndex]
+  })
 }
 
 function blurCell () {
+  this.setState({
+    focused: null
+  })
 }
 
 function removeRow (ev) {
+  ev.preventDefault()
+  if (this.state.table.length > 1) {
+    var tableClone = this.state.table.slice(0, -1) 
+    this.setState({
+      table: tableClone
+    })
+  }
 }
 
 function removeColumn (ev) {
+  ev.preventDefault()
+  if (this.state.table[0].length > 1) {
+    var newTable = this.state.table.map((array) => array.slice(0, -1))
+    this.setState({
+      table: newTable
+    })
+  }
 }
 
 module.exports = {
--- components/Cell.js
+++ components/Cell.js
@@ -3,7 +3,15 @@
 const React = require('react');
 
 class Cell extends React.Component {
+  constructor() {
+    super();
+  } 
+
   render () {
+    return (<td className="cell"> 
+      <input type="text" onChange={this.props.onChange} value={this.props.cell} onFocus={this.props.onFocus}> </input> 
+    </td>
+    )
   }
 }
 
