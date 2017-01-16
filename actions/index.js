9907c3dd6f260686d04b52e52242557eb7e967de
Wendy Xu <guangw.xu@gmail.com>
2017-01-15 19:59:55 -0500

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,24 +1,63 @@
 'use strict';
 
 function addColumn (ev) {
+  ev.preventDefault();
+  this.setState({
+    table: this.state.table.map((row, index) => {
+      return [...row, ''];
+    })
+  });
 }
 
 function addRow (ev) {
+  ev.preventDefault();
+  const currentTable = this.state.table;
+  this.setState({
+    table: [...currentTable, Array(currentTable[0].length).fill('')]
+  });
 }
 
 function changeCell (rowIndex, columnIndex, ev) {
+  const changedCell = ev.target.value;
+  let changingTable = this.state.table;
+  changingTable[rowIndex][columnIndex] = changedCell;
+
+  this.setState({
+    table: changingTable
+  });
 }
 
 function focusCell (rowIndex, columnIndex) {
+  this.setState({
+    focused: [rowIndex, columnIndex]
+  });
 }
 
 function blurCell () {
+  this.setState({
+    focused: null
+  });
 }
 
 function removeRow (ev) {
+  ev.preventDefault();
+  const currentTable = this.state.table;
+  if(currentTable.length > 1){
+    this.setState({
+      table: currentTable.splice(0, currentTable.length-1) 
+    });
+  }
 }
 
 function removeColumn (ev) {
+  ev.preventDefault();
+  if(this.state.table[0].length > 1){
+    this.setState({
+      table: this.state.table.map((row, index) => {
+        return row.splice(0, row.length-1);
+      })
+    });
+  }
 }
 
 module.exports = {
--- components/Table.js
+++ components/Table.js
@@ -5,6 +5,21 @@ const Row = require('./Row');
 
 class Table extends React.Component {
   render () {
+    const rows = this.props.table.map((row, index) => {
+      return <Row
+              key={index}
+              row={row}
+              onChange={this.props.onChange.bind(null, index)}
+              onFocus={this.props.onFocus.bind(null, index)}
+            /> 
+    });
+    return (
+      <table className="table" onBlur={this.props.onBlur} >
+        <tbody>
+          {rows}
+        </tbody>
+      </table>    
+    );
   }
 }
 
