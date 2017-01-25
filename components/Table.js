aae95cea820de7cddea8b4b0a2c5643ab3cfe92b
Flatiron School <github email address>
2017-01-25 18:20:37 -0500

    done

--- components/Table.js
+++ components/Table.js
@@ -5,6 +5,22 @@ const Row = require('./Row');
 
 class Table extends React.Component {
   render () {
+    const rows = this.props.table.map((row, i) =>
+    <Row
+      key={i}
+      row={row}
+      onChange={this.props.onChange.bind(null, i)}
+      onFocus={this.props.onFocus.bind(null, i)}
+    />
+  );
+
+    return (
+      <table className="table" onBlur={this.props.onBlur}>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    )
   }
 }
 
