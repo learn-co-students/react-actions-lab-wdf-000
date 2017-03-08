573b0dab9f2a847d12fed539416e196c6979cf59
sami4thewin <samiomarmohammed@gmail.com>
2017-03-08 06:26:32 -0500

    Working on the final actions tests

--- components/Row.js
+++ components/Row.js
@@ -7,7 +7,7 @@ class Row extends React.Component {
   render () {
     return(
       <tr className="row">
-        {this.props.row.map(item => <Cell cell={item} onChange={} onFocus={}/>)}
+        {this.props.row.map((cell, columnIndex) => <Cell key={columnIndex} cell={cell} onChange={this.props.onChange.bind(null, columnIndex)} onFocus={this.props.onFocus.bind(null, columnIndex)}/>)}
       </tr>
     )
   }
